b3beb221be8d8092e9b84af3b725d72fe6d1567f
unknown <santiago.cordoba-d@labinfo.is.escuelaing.edu.co>
2025-10-21 09:35:31 -0500

    funcion primes funcional

--- MathServices/src/main/java/parcial/arep/MathServices/MathService.java
+++ MathServices/src/main/java/parcial/arep/MathServices/MathService.java
@@ -23,10 +23,16 @@ public class MathService {
 
     @GetMapping("/primes")
     public Map<String, Object> primes(@RequestParam Integer value){
-        Map<String,Object> response = new HashMap<>();
-        List<Integer> result = new ArrayList<Integer>();
+        Map<String,Object> response = new LinkedHashMap<>();
+        List<Integer> intList = new ArrayList<Integer>();
+        for(int i = 2; i <= value; i++){
+            if(isFactor(i)){
+                intList.add(i);
+            }
+        }
         response.put("Operation", "primes");
         response.put("Input", "value");
+        response.put("Output", intList);
         return response;
     }
 
@@ -36,5 +42,16 @@ public class MathService {
                 intList.add(i);
             }
         }
+    }
+
+    private boolean isFactor(Integer value){
+        List<Integer>listFacts = new ArrayList<>();
+        for(int i = 1; i <= value; i++){
+            if(value%i==0){
+                listFacts.add(i);
+            }
+        }
+        return listFacts.size() == 2;
+
     }
 }

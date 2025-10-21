65c478c96866403833f92063c5d6441f25942df0
unknown <santiago.cordoba-d@labinfo.is.escuelaing.edu.co>
2025-10-21 08:50:05 -0500

    Se realizo la creación del los proyectos con Spring Initilizr

---
+++ MathServices/src/main/java/parcial/arep/MathServices/MathServicesApplication.java
@@ -0,0 +1,13 @@
+package parcial.arep.MathServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathServicesApplication.class, args);
+	}
+
+}
---
+++ MathServices/src/test/java/parcial/arep/MathServices/MathServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package parcial.arep.MathServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
